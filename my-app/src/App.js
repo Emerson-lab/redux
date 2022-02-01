a9075a2c78f92475e6fdaf276421450afb5d1ddb
Emerson-lab <trinddade29@gmail.com>
2022-01-31 22:30:38 -0300

    declando app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./routes";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
 }
